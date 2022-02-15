a188795c6068ae9dbe30d99a8d612db274accbf9
ssbhattarai <nascentsurya@gmail.com>
2022-02-15 13:52:24 +0545

    changes

---
+++ js/main.js
@@ -0,0 +1,79 @@
+
+
+// change text function
+
+// function changeText()
+// {
+//     document.getElementById('test').style.color= 'red';
+// }
+
+
+// object //array
+
+// let personalInformation = {
+//     Name : 'SHyam',
+//     "age" : 30,
+//     "isMarried": false
+// }
+
+// console.log(typeof(personalInformation))
+
+// console.log(personalInformation.age)
+
+// array
+// let  num = [1,2,3,4,5,6,7,8];
+
+// console.log(typeof)
+// console.log(num[7])
+
+
+// DOM Document Object MOdel
+
+// let doc = document
+// doc  = document.links
+// doc = document.links[0].href
+
+// doc = document.forms[0]
+
+
+// console.log(doc);
+
+
+// Types of selectors
+// 1. Single
+    // let element = document.getElementById('test');
+    // element.style.fontSize = "25px";
+    // element.innerText = "Hello ";
+    // element.innerHTML="<i>Hello</i>"
+
+    // console.log(element);
+
+
+    // let el = document.querySelector('.heading');
+    // el.style.fontSize = "25px";
+    // el.innerHTML="<i>Hello</i>"
+
+    // console.log(el)
+
+
+// 2. Multiple
+    // let elements = document.getElementsByClassName('heading');
+    // console.log(elements[0]);
+
+
+// function add(x)
+// {
+//     let element = document.getElementById('test');
+//     element.style.fontSize = "25px";
+//     element.style.color = 'red';
+//     element.innerHTML = "kjsdfljkasd kjasdlfkaj" ;
+// }
+
+
+
+
+
+
+
+
+
